045c476a14d1dfd7ed1378c16f7dc7ac1a93e729
Divyam Mishra <divyammishra0918@gmail.com>
2024-01-06 16:50:47 +0530

    time changed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.listen(3001, () => {
 
 let isFetching = false; // Flag to track ongoing fetch operation
 
-cron.schedule('46 16 * * *', async () => {
+cron.schedule('51 16 * * *', async () => {
   if (!isFetching) {
     isFetching = true; // Set flag to true to indicate an ongoing fetch operation
 
