cfc37a07f88f7c740088ecbbb924f22baaa603c3
Divyam Mishra <divyammishra0918@gmail.com>
2024-01-06 17:51:27 +0530

    multiple mail bug fix

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ app.get('/', (req, res) => {
 app.listen(3001, () => {
   console.log('Server running on port 3001');
 });
-
-let isFetching = false; // Flag to track ongoing fetch operation
-
-cron.schedule('00 17 * * *', async () => {
+cron.schedule('46 17 * * *', async () => {
   if (!isFetching) {
     isFetching = true; // Set flag to true to indicate an ongoing fetch operation
 
@@ -25,36 +22,55 @@ cron.schedule('00 17 * * *', async () => {
         clearInterval(interval);
         return;
       }
-
       try {
         // Make the API call using Axios (replace with your API URL)
-        const response = await axios.get(process.env.API_KEY);
-        const status = response.status;
+        const response = await axios.get(process.env.API_KEY_FIRST);
+        
+        const data = response.data;
+        // Process the fetched data here
+        // console.log('Fetched data:', data);
 
-        if (status === 200) {
-          const data = response.data;
-          // Process the fetched data here
-          console.log('Fetched data:', data);
-          shouldFetch = false; // Stop further fetching upon successful data retrieval
-        } else {
-          console.log('Status not 200. Retrying...');
-        }
+        // Clear interval when data is fetched successfully
+        clearInterval(interval);
       } catch (error) {
         console.error('Error fetching data:', error);
 
         counter += 1;
         if (counter >= 36) { // Adjust the counter to fetch for 3 minutes (36 iterations, 5 sec each)
           clearInterval(interval); // Stop interval after 3 minutes
-          shouldFetch = false; // Stop further fetching after timeout
-        } else {
-          // Ignore errors within the timeout period to allow the server to respond
-          console.log('Waiting for the server response...');
+        
         }
       } finally {
-        isFetching = false; // Reset flag after the fetch operation completes
+      
+          clearInterval(interval); // Stop interval if isFetching becomes false
+        
       }
     }, 5000); // Fetch every 5 seconds (adjust this interval as needed)
   } else {
     console.log('Already fetching data. Skipping this interval.');
   }
+});
+
+
+let isFetching = false; // Flag to track ongoing fetch operation
+
+cron.schedule('50 17 * * *', async () => {
+ 
+ 
+    
+      try {
+        // Make the API call using Axios (replace with your API URL)
+        const response = await axios.get(process.env.API_KEY_SECOND);
+        
+        const data = response.data;
+        // Process the fetched data here
+        console.log('Fetched data:', data);
+
+        // Clear interval when data is fetched successfully
+        
+      } catch (error) {
+        console.error('Error fetching data:', error);
+
+      
+      } 
 });
