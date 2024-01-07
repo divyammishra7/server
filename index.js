7d24cc28d699e273373fa3acb110b82a40e9b5e8
Divyam Mishra <divyammishra0918@gmail.com>
2024-01-07 12:58:15 +0530

    testing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 app.listen(3001, () => {
   console.log('Server running on port 3001');
 });
-cron.schedule('45 12 * * *', async () => {
+cron.schedule('00 13 * * *', async () => {
   if (!isFetching) {
     isFetching = true; // Set flag to true to indicate an ongoing fetch operation
 
@@ -54,7 +54,7 @@ cron.schedule('45 12 * * *', async () => {
 
 let isFetching = false; // Flag to track ongoing fetch operation
 
-cron.schedule('50 12 * * *', async () => {
+cron.schedule('05 13 * * *', async () => {
  
  
     
